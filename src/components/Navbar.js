2db0feff4e9bc48b45859832c3320910841472e7
Johann <johanson88@gmail.com>
2020-09-08 15:35:31 +0200

    navbar working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,46 @@
 //Navbar.js
-import React from 'react';
+import React, {useEffect} from 'react';
 import './navbar.scss';
-import Logo from './../img/logo.svg';
+//import Logo from './logo.svg';
 const Navbar=() => {
+    const [scrolled,setScrolled]=React.useState(false);
+
+    const handleScroll=() => {
+        console.log("handling");
+  
+      const offset=window.scrollY;
+      console.log(offset);
+  
+      if(offset > 200 ){
+  
+        setScrolled(true);
+  
+      }
+  
+      else{
+  
+        setScrolled(false);
+  
+      }
+  
+    }
+  
+    useEffect(() => {
+        console.log("scrolling!!!!")
+        window.addEventListener('scroll',handleScroll)
+  
+    })
+  
+    let navbarClasses=['navbar'];
+  
+    if(scrolled){
+  
+      navbarClasses.push('scrolled');
+  
+    }
+
   return (
-    <header className={navbarClasses.join(" ")}>
+    <header className={navbarClasses.join(' ')}>
 
         <div className="logo">
             {/* your logo */}
